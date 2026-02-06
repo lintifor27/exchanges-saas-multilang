706bfe39758d2144c114fe6d4a2d66e3aeaada30
lintifor27 <dostapcuk42@gmail.com>
2026-02-05 23:15:06 -0800

    Remove experimental appDir property from next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   i18n: {
     locales: ['en', 'ua', 'ru'],
     defaultLocale: 'en',
   },
 };
-
 export default nextConfig;
