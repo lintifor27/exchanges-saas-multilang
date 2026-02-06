1c5e89538487f6f6bb7ebbffd40f7ed9458fd266
lintifor27 <dostapcuk42@gmail.com>
2026-02-06 11:09:23 +0200

    Add files via upload

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Removed deprecated experimental.appDir option. The app directory is enabled by default
   i18n: {
     locales: ['en', 'ua', 'ru'],
     defaultLocale: 'en',
   },
 };
+
 export default nextConfig;
